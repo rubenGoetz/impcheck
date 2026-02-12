536c3addab3ed46db825b1fa0df289a4baa59de5
Ruben Götz <ruben.goetz@abgoetz.de>
2026-02-12 10:29:12 +0100

    adapted benchmark script to local disc

--- scripts/run/benchmark.sh
+++ scripts/run/benchmark.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-spack env activate mallob
+# spack env activate mallob
 
-benchmark_instances_path="../benchmarks"
-proofs_dir_in="proof_palrup"
-proofs_dir_out="proof_working"
-num_solvers=64
+benchmark_instances_path="../short_benchmarks"
+proofs_dir_in="/local_scratch/proof_palrup"
+proofs_dir_out="/local_scratch/proof_working"
+num_solvers=16
 processors=2
 log_dir="proof_log"
-mallob_path="../mallob"
+mallob_path="../mallob_clean/mallob"
 
 for benchmark in $(ls $benchmark_instances_path); do
     
@@ -17,7 +17,7 @@ for benchmark in $(ls $benchmark_instances_path); do
     bash scripts/run/run_complete_pipeline.sh \
         -proof-dir-in="$proofs_dir_in/$benchmark" -proof-dir-out="$proofs_dir_out/$benchmark" \
         -num-solvers=$num_solvers -processors=$processors -formula-path=$benchmark_instances_path/$benchmark \
-        -log-dir="$log_dir/$benchmark" -run-mallob=$mallob_path -c=2    
+        -log-dir="$log_dir/$benchmark" -palrup-binary=1 -c=2 -run-mallob=$mallob_path
     echo ""
 
 done
